1816f9c4326eed1f398f7292975d9b01f8feced5
dev-rj3 <dev.rj3@outlook.com>
2020-05-04 16:26:55 -0700

    First Commit

---
+++ game.py
@@ -0,0 +1,44 @@
+from random import randint
+
+
+class GuessGame:
+
+    def __init__(self, lower_bound=1, upper_bound=100):
+        self.__guesses = 1
+        self.__MIN = lower_bound
+        self.__MAX = upper_bound
+        self.__NUMBER = randint(self.__MIN, self.__MAX)
+
+    def play(self):
+        while 1:
+
+            number = self.__get_guess()
+
+            if number > self.__NUMBER:
+                print('Guess is too high.')
+            elif number < self.__NUMBER:
+                print('Guess is too low.')
+            else:
+                break
+            self.__guesses += 1
+
+        print(f'Guessed with {self.__guesses}')
+
+    def __get_guess(self):
+        response = input(
+            f'Guess a number from {self.__MIN} to {self.__MAX}: ')
+
+        parsed_response = self.__is_valid_number(response)
+        if (parsed_response):
+            number = int(response)
+            return number
+
+        print("Please enter a valid number")
+        return self.__get_guess()
+
+    def __is_valid_number(self, response):
+        try:
+            number = int(response)
+        except:
+            return False
+        return self.__MIN <= number <= self.__MAX
---
+++ main.py
@@ -0,0 +1,3 @@
+from game import GuessGame
+
+GuessGame(1, 10).play()
